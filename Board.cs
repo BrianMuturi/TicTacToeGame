b204cb1ca3800e3e3eee1c9670c2ae056cfe8538
Brian Muturi <muturibrian18@gmail.com>
2021-02-05 02:43:22 +0100

    Classes

---
+++ Board.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    public class Board
+    {
+        public Row[] rows;
+        public Board()
+        {
+            rows = new Row[3];
+            for (int i = 0; i < 3; i++)
+                rows[i] = new Row();
+        }
+        public void ShowBoard()
+        {
+            for (int i = 0; i < rows.Length; ++i)
+                rows[i].DisplayContents();
+        }
+        //User input
+        public void ChooseRowThenSpace()
+        {
+            string input = "";
+            int rowNumber = 0;
+            Console.WriteLine("Type a row number from top to bottom (1, 2, or 3)");
+            input = Console.ReadLine();
+            if (int.TryParse(input, out rowNumber))
+            {
+                if (rowNumber > 3 || rowNumber < 1)
+                {
+                    Console.WriteLine("Invalid row number. Please try again!!");
+                    ChooseRowThenSpace();
+                    return;
+                }
+                rows[rowNumber - 1].ChooseSpace(TicTacToeGame.isPlayer1sTurn);
+            }
+            else
+            {
+                Console.WriteLine("Invalid row number. Try again!!");
+                ChooseRowThenSpace();
+                return;
+            }
+        }
+    }
+}
---
+++ Row.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeGame
+{
+    public class Row
+    {
+        public int[] spaces;
+
+        public void ChooseSpace(bool isX)
+        {
+            string input;
+            int spaceNumber;
+            Console.WriteLine("Type a space number from left to right (1, 2, or 3)");
+            input = Console.ReadLine();
+            if (int.TryParse(input, out spaceNumber))
+            {
+                if (spaceNumber > 3 || spaceNumber < 1)
+                {
+                    Console.WriteLine("Invalid space number. Please try again!!");
+                    ChooseSpace(isX);
+                    return;
+                }
+                if (spaces[spaceNumber - 1] != 0)
+                {
+                    Console.WriteLine("That space is occupied. Please choose another!!");
+                    TicTacToeGame.board.ChooseRowThenSpace();
+                    return;
+                }
+
+                if (isX)
+                    spaces[spaceNumber - 1] = 1;
+                else
+                    spaces[spaceNumber - 1] = 2;
+            }
+            else
+            {
+                Console.WriteLine("Invalid space number. Try again!!");
+                ChooseSpace(isX);
+            }
+        }
+        //Display results
+
+        public void DisplayContents()
+        {
+            string lineToWrite = "";
+            for (int i = 0; i < spaces.Length; ++i)
+            {
+                if (spaces[i] == 0)
+                    lineToWrite += "|   |";
+                else if (spaces[i] == 1)
+                    lineToWrite += "| X |";
+                else if (spaces[i] == 2)
+                    lineToWrite += "| O |";
+
+            }
+
+            Console.WriteLine(lineToWrite);
+        }
+
+        public Row()
+        {
+            spaces = new int[3];
+        }
+    }
+}
